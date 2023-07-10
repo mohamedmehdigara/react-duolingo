588f753984ca12d7580a85ea4e410f67d863ef5d
mohamedmehdigara <garamehdi57@yahoo.com>
2023-07-10 11:37:26 +0100

    added files

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ProgressContext } from '../ProgressContext';
 
 const Profile = () => {
+  const { progress } = useContext(ProgressContext);
+
   return (
     <div>
       <h1>Profile Page</h1>
+      <h2>Profile Information</h2>
+      {/* Display user's profile information */}
+      <h2>Achievements</h2>
+      {/* Display user's achievements */}
+      <h2>Progress</h2>
+      <p>Current progress: {progress}%</p>
     </div>
   );
 };
