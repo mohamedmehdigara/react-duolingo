9b75a4c0bec6483ab8b4feafbedd9e6aafd8f5c8
mohamedmehdigara <garamehdi57@yahoo.com>
2023-07-10 12:51:55 +0100

    added files

--- src/pages/Lessons.js
+++ src/pages/Lessons.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import Lesson from '../components/Lesson';
+import React, { useState } from 'react';
 
 const Lessons = () => {
+  const [completedLessons, setCompletedLessons] = useState([1, 3]); // Example: Completed lessons IDs
+
   const lessons = [
     { id: 1, title: 'Lesson 1' },
     { id: 2, title: 'Lesson 2' },
@@ -9,12 +10,32 @@ const Lessons = () => {
     // Add more lessons as needed
   ];
 
+  const isLessonCompleted = (lessonId) => {
+    return completedLessons.includes(lessonId);
+  };
+
+  const handleLessonClick = (lessonId) => {
+    // Toggle lesson completion status
+    const updatedCompletedLessons = completedLessons.includes(lessonId)
+      ? completedLessons.filter((id) => id !== lessonId)
+      : [...completedLessons, lessonId];
+    setCompletedLessons(updatedCompletedLessons);
+  };
+
   return (
     <div>
       <h1>Lessons Page</h1>
-      {lessons.map((lesson) => (
-        <Lesson key={lesson.id} title={lesson.title} />
-      ))}
+      <div className="lessons-container">
+        {lessons.map((lesson) => (
+          <div
+            key={lesson.id}
+            className={`lesson ${isLessonCompleted(lesson.id) ? 'completed' : ''}`}
+            onClick={() => handleLessonClick(lesson.id)}
+          >
+            <p>{lesson.title}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
